45681cf63925b5f356ab8fc90158a336762537b3
cassandra-mzizi <cassandra.mzizi04@gmail.com>
2024-07-30 15:01:13 +0200

    Routes Completed v-1

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+include __DIR__ . "/../src/App/functions.php";
 $app = require __DIR__ . "/../src/App/bootstrap.php";
 
 $app->run();
+
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 require __DIR__ . '/../../vendor/autoload.php';
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
+
+$app->get('/', [homeController::class, 'home']);
 
 return $app;
